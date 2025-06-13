bcc6a00ef843ffa24e9713806cec2ad83d4f222b
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2025-06-13 13:05:25 +0300

    -connect To Db (mongoDB)

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
-import  express from 'express'
-const app = express()
-const port = 3000
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+import http from 'http';
+import express from 'express';
+import connectToMongoDb from './db/connectToMongoDb.js';
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello from Takasta backend!'));
+
+async function initializeApp() {
+  try {
+    const mongoUri = process.env.MONGO_DB_URI;
+    if (!mongoUri) throw new Error('MONGO_DB_URI is not defined');
+    await connectToMongoDb(mongoUri);
+
+    const PORT = process.env.PORT || 3001;
+    const server = http.createServer(app);
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log(`Frontend URL: ${process.env.FRONTEND_URL}`);
+    });
+  } catch (err) {
+    console.error('Critical error while starting the application:', err);
+  }
+}
+
+initializeApp();
